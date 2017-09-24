1d69282764dfb2d299a81d4511c8b70df88b4bf6
risman94 <kamarulrisman1994@gmail.com>
2017-09-24 08:39:12 +0800

    commit wae

--- src/app/index.js
+++ src/app/index.js
@@ -4,16 +4,55 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            homeLink: "Home",
+            homeMounted: true
+        };
+    }
+
     onGreet() {
         alert("hello!");
     }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        });
+    };
+
+    onChangeHomeMounted() {
+        this.setState({
+            homeMounted: !this.state.homeMounted
+        });
+    }
+
     render () {
+        let homeCmp="";
+        if(this.state.homeMounted) {
+            homeCmp = (
+                < Home 
+                    name={"max"} 
+                    initialAge={23} 
+                    greet={this.onGreet}
+                    changeLink={this.onChangeLinkName.bind(this)}
+                    initialLinkName={this.state.homeLink}
+                />
+            );
+        }
         return (
             <div className="container">
-                < Header homeLink = "Home" />
+                < Header homeLink={this.state.homeLink} />
                 <div className="container">
                     <hr/>
-                    < Home name={"max"} initialAge={23} greet={this.onGreet} />
+                    {homeCmp}
+                </div>
+                <div className="container">
+                    <hr/>
+                    <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-dark">
+                        (Un)Mount Home Component
+                    </button>
                 </div>
             </div>
         );

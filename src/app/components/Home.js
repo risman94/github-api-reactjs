19dda13384843000f3ea53752fd6e97b2ca796a5
risman94 <kamarulrisman1994@gmail.com>
2017-09-26 12:01:14 +0800

    add search user github and modifet css

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,57 +1,72 @@
 import React from "react";
 import Axios from "axios";
 
+let timeoutId;
+
 export class Home extends React.Component {
   constructor(props) {
     super();
     this.state = {
-      user: null,
+      fetch: false,
+      user: "",
       value: ""
     };
   }
 
   handleChange(e) {
     this.setState({
-      value: e.target.value
+      value: e.target.value,
+      fetch: true
     });
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => this.handleRequest(), 1000);
   }
 
-  handle(e) {
-    e.preventDefault();
+  handleRequest() {
     const api = "https://api.github.com/search/users";
-    Axios.get(api + "?q=" + this.state.value)
-      .then(response => {
-        console.log(response.data.items);
-        this.setState({
-          user: response.data.items
+    console.log("ini pas request");
+    if (this.state.value) {
+      Axios.get(`${api}?q=${this.state.value}`)
+        .then(response => {
+          console.log(response.data.items, "ini di axios");
+          this.setState({
+            user: response.data.items,
+            fetch: false
+          });
+        })
+        .catch(e => {
+          console.log(e);
         });
-      })
-      .catch(e => {
-        console.log(e);
+    }
+    if (!this.state.value) {
+      this.setState({
+        user: ""
       });
+    }
   }
-
+  renderList() {
+    if (!this.state.fetch) {
+      return this.state.user.map((tampil, index) => (
+        <div key={index} className="col-md-3 thumbnail">
+          <h3>{tampil.login}</h3>
+          <img src={tampil.avatar_url} className="img-thumbnail" />
+        </div>
+      ));
+    } else {
+      return <div> Loading .. </div>;
+    }
+  }
   render() {
-    // let filterUser;
-    // if (this.state.user !== null) {
-    //   filterUser = this.state.user.filter(user => {
-    //     return user.login.indexOf(this.state.value) !== -1;
-    //   });
-    // }
     return (
       <div className="container">
         <h3>Home</h3>
-        <form onSubmit={this.handle.bind(this)}>
-          <input type="text" onChange={this.handleChange.bind(this)} />
-          <input type="submit" value="Submit" />
-        </form>
-        {this.state.user !== null ? (
-          this.state.user.map((tampil, index) => (
-            <div key={index}>{tampil.login}</div>
-          ))
-        ) : (
-          <div> Loading .. </div>
-        )}
+        <input
+          className="form-control"
+          placeholder="Search user github ..."
+          type="text"
+          onChange={this.handleChange.bind(this)}
+        />
+        {this.state.user ? this.renderList() : ""}
       </div>
     );
   }
